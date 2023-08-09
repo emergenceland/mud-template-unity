fb4700e1a29d7b54e107848ad2aa3425fad48fb6
Neilson K-S <neilson.ks@gmail.com>
2023-08-09 09:28:54 -0700

    stress test scene

---
+++ packages/client/Assets/Example/Scripts/StressTestManager.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using IWorld.ContractDefinition;
+using mud.Unity;
+using UniRx;
+using DefaultNamespace;
+using mud.Client;
+using UnityEngine;
+using ObservableExtensions = UniRx.ObservableExtensions;
+
+public class StressTestManager : MonoBehaviour
+{
+    private IDisposable _counterSub;
+    public GameObject prefab;
+    private NetworkManager net;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        net = NetworkManager.Instance;
+        net.OnNetworkInitialized += SubscribeToCounter;
+    }
+
+    private void SubscribeToCounter(NetworkManager _)
+    {
+        var incrementQuery = new Query().In(CounterTable.TableId);
+        _counterSub = ObservableExtensions.Subscribe(net.ds.RxQuery(incrementQuery).ObserveOnMainThread(), OnIncrement);
+    }
+
+
+    private void OnIncrement((List<Record> SetRecords, List<Record> RemovedRecords) update)
+    {
+        // first element of tuple is set records, second is deleted records
+        foreach (var record in update.SetRecords)
+        {
+            var currentValue = record.value;
+            if (currentValue == null) return;
+            Debug.Log("Counter is now: " + currentValue["value"]);
+            SpawnPrefab();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SendIncrementTxAsync().Forget();
+        }
+    }
+
+    private async UniTaskVoid SendIncrementTxAsync()
+    {
+        try
+        {
+            await net.worldSend.TxExecute<IncrementFunction>();
+        }
+        catch (Exception ex)
+        {
+            // Handle your exception here
+            Debug.LogException(ex);
+        }
+    }
+
+    private void SpawnPrefab()
+    {
+        var randomX = UnityEngine.Random.Range(-1, 1);
+        var randomZ = UnityEngine.Random.Range(-1, 1);
+        Instantiate(prefab, new Vector3(randomX, 5, randomZ), Quaternion.identity);
+    }
+
+    private void OnDestroy()
+    {
+        _counterSub?.Dispose();
+    }
+}
